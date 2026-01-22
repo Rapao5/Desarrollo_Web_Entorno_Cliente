b659d9c3fc6323c1bcabb9840e41b840d564534d
Rapao5 <2061045@alu.murciaeduca.es>
2026-01-22 17:18:27 +0100

    terminado

--- nivel1_react/my-react-app/src/App.jsx
+++ nivel1_react/my-react-app/src/App.jsx
@@ -1,17 +1,44 @@
-import {useState} from "react"
+import {useState} from 'react'
 
-function ParentComponent() {
-    const [name, setName] = useState("Juanaco")
-    return <ChildComponent name={name} setName={setName} />
-}
-function ChildComponent(props){
+function App(){
+    const [isParagraphVisible, setIsParagraphVisible] = useState(true)
+    const toggleStatus = () => {
+    setIsParagraphVisible(!isParagraphVisible)
+    }
+
+    const [newContador, setNewContador] = useState(0)
+    const [modo, setModo] = useState()
+
+    const [estadoBoton, setEstadoBoton] = useState(true)
+    const cambiarBoton = () => {
+        setEstadoBoton(!estadoBoton)
+    }
+ 
     return (
         <>
-            <h1>Hello {props.name}</h1>
-            <button onClick={ () => props.setName("Juanky")}> Change name</button>
-            <button onClick={ () => props.setName("Klara")}> Saluda a la jefa</button>
+        <h1>Miniretillo</h1>
+        {isParagraphVisible && (
+        <p>Este es un texto oculto.</p>
+        )}
+        <button onClick={toggleStatus}>
+        {isParagraphVisible ? 'Esconder' : 'Mostrar'} texto
+        </button>
+        <p>Contador {newContador}</p>
+        <button onClick={() => setNewContador((n) => n + 1)}>+1
+        numero</button>
+        <button onClick={() => setNewContador(0)}>Reset</button>
+        <br/>
+        <br/>
+        <input type="text" value={modo} placeholder='Añade algo (papafrita)' onChange={(e)=>setModo(e.target.value)}/>
+        {modo && <p>{modo}</p>}
+        <br/>
+        <br/>
+        {estadoBoton && (
+            <p>Este botón ya estaba al inicio.</p>
+        )}
+        <button onClick={cambiarBoton}>{estadoBoton ? 'Ocultar' : 'Mostrar'}</button>
         </>
     )
 }
 
-export default ParentComponent
+export default App
