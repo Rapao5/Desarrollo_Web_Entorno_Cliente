d1d1cc623002054e92f90cd76e689a962b7421b6
Rapao5 <2061045@alu.murciaeduca.es>
2026-01-26 19:39:01 +0100

    terminar

--- nivel1_react/my-react-app/src/App.jsx
+++ nivel1_react/my-react-app/src/App.jsx
@@ -1,17 +1,73 @@
+import { useState } from 'react';
 import './style.css'; 
 
 function App() {
-return (
-<div className='centrar'>
-<h1 className='text-3xl font-bold'>Klara la jefa</h1>
-<p className='mt-2 text-sm opacity-80'>
-Foros de programación
-</p>
-<button className='mt-4 px-4 py-2 text-white bg-blue-500 rounded paragraph-text'>
-Entrar
-</button>
-</div>
-);
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const handleSubmit = (e) => {
+        e.preventDefault(); 
+        if (usernameError || emailError || passwordError) {
+            alert('No se puede enviar: el formulario contiene errores');
+        } else {
+            alert(`${username}, ${email}, ${password}`);
+        }
+        };
+        const [usernameError, setUsernameError] = useState('');
+        const handleUsername = (e) => {
+            const { value } = e.target;
+            setUsername(value);
+            if (value.length <= 6) {
+                setUsernameError('El username debe tener más de 6 caracteres');
+            } else {
+                setUsernameError('');
+            }
+        };
+        const [emailError, setEmailError] = useState('');
+        const handleEmail = (e) => {
+            const { value } = e.target;
+            setEmail(value);
+            if (!value.includes('@') || !value.includes('.')) {
+                setEmailError('El email debe contener @ y un .');
+            } else {
+                setEmailError('');
+            }
+        };
+        const [passwordError, setPasswordError] = useState('');
+        const handlePassword = (e) => {
+            const { value } = e.target;
+            setPassword(value);
+            if (value.length<8){
+                setPasswordError('La contraseña debe tener mínimo 8 caracteres.');
+            }else{
+                setPasswordError('');
+            }
+        }
+    return (
+            <form className='centrar' onSubmit={handleSubmit}>
+                Username:
+                <input
+                    type="text"
+                    value={username}
+                    onChange={handleUsername}
+                />
+                Email:
+                <input
+                    type="email"
+                    value={email}
+                    onChange={handleEmail}
+                />
+                Password:
+                <input
+                    type="password"
+                    value={password}
+                    onChange={handlePassword}
+                />
+                <button>Submit</button>
+                <p className='error'>{usernameError}</p>
+                <p className='error'>{emailError}</p>
+                <p className='error'>{passwordError}</p>
+            </form>
+        );
 }
-
-export default App
+export default App;
